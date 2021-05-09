f6d17b2acc2effcc98eade1c32bbda31f1fb4f75
Dima Kukhta <dima.kukhta.2001@mail.ru>
2021-05-09 15:44:05 +0300

    fix: some errors

--- caesar/program/error.js
+++ caesar/program/error.js
@@ -6,9 +6,11 @@ const error = (action, shift, input, output) => {
         process.exit(1);
     }
 
-    if (input === output) {
-        process.stderr.write('Error: Input file and Output file have the same name');
-        process.exit(1);
+    if (input && output) {
+        if (input === output) {
+            process.stderr.write('Error: Input file and Output file have the same name');
+            process.exit(1);
+        }
     }
 
     if (input) {
